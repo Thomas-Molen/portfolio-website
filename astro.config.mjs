21250b3838a810602aa0c1dc5c0b3326aa4b5c72
Thomas Molen <sugarypineapple2@gmail.com>
2023-08-13 23:35:12 +0200

    Add Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'server',
+  integrations: [react()],
+  adapter: vercel({
+    analytics: true,
+  })
 });
