3cf53b901b6efa3bc1fa8bb2a828037b9d51ee43
Thomas Molen <sugarypineapple2@gmail.com>
2023-08-13 23:53:08 +0200

    disable vercel webanalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [react()],
-  adapter: vercel({
-    analytics: true,
-  }),
-  vite: {
-    define: {
-      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-    },
-  },
+  integrations: [react()]
 });
