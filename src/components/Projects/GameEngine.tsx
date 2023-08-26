74bbd35c9095940e2f7c0631fef5a5f2eb36929a
Thomas Molen <sugarypineapple2@gmail.com>
2023-08-26 19:18:30 +0200

    Add source code links to public projects

--- src/components/Projects/GameEngine.tsx
+++ src/components/Projects/GameEngine.tsx
@@ -5,6 +5,9 @@ function GameEninge() {
         <>
             <div className="d-flex justify-content-between align-items-end">
                 <h3>Game Eninge</h3>
+                <Underline initialWidth="0">
+                    <a href="https://github.com/Thomas-Molen/SugaryEngine" target="_blank"><i className="bi bi-git me-1"></i>Source code</a>
+                </Underline>
             </div>
             <hr className="my-0" />
             <p>
