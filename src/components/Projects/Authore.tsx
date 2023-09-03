07ce3d50a1bc5d31280ab6029715e005b3679658
Thomas Molen <sugarypineapple2@gmail.com>
2023-09-03 22:01:43 +0200

    Added Author-e project description

--- src/components/Projects/Authore.tsx
+++ src/components/Projects/Authore.tsx
@@ -5,7 +5,7 @@ function Authore() {
     return (
         <>
             <div className="d-flex justify-content-between align-items-end">
-                <h3>Author-e Job</h3>
+                <h3>Author-e</h3>
             </div>
             <hr className="my-0" />
             <div className="my-1 mb-2 row row-cols-auto gx-1">
@@ -16,9 +16,12 @@ function Authore() {
                 </span>
             </div>
             <p className="project-intro">
+                Lead developer for several business oriented projects.
             </p>
-            <p>
-            </p>
+            <p>At Author-e, a dynamic software company, I actively contributed to many projects for various businesses. My primary focus was on creating efficient and maintainable software solutions.</p>
+            <p>My work at Author-e allowed me to take a leading role in delivering purpose built software solutions. I architected new systems for complex business solutions as well as helped maintain existing solutions for our partners.</p>
+            <p>Engaging with stakeholders within client organizations was a crucial to the success of any project I worked on. I actively collaborated with them to gather requirements, provide progress updates, and address any evolving needs or concerns.</p>
+            <p>Building strong relationships with clients and ensuring their satisfaction was a fundamental part of my work at Author-e.</p>
         </>
     );
 }
