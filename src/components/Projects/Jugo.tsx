561f37f56c03f3e77da6c9b736a7057af3acc373
Thomas Molen <sugarypineapple2@gmail.com>
2023-09-03 22:46:36 +0200

    Added Jugo project description

--- src/components/Projects/Jugo.tsx
+++ src/components/Projects/Jugo.tsx
@@ -15,9 +15,12 @@ function Jugo() {
                 <Technology image="scikit.png" alt="Python SciKit Learn & Surprise" />
             </div>
             <p className="project-intro">
+                Predicting advantageous travel times for Jugo consultants, helping the environment and productivity.
             </p>
-            <p>
-            </p>
+            <p>As part of a multi-disciplinary team, I had the opportunity to collaborate on an impactful project for the consultancy firm Jugo. Uniting a team with diverse skills for a common goal; empowering Jugo within the world of Artificial Inteligence.</p>
+            <p>Our task was to acquire and analyze extensive datasets, culminating in the development of a machine learning model.<br />
+            This model was developed with the purpose of predicting the most advantageous travel times for Jugo's consultants when they work on-site at client locations.</p>
+            <p>Together with my teammates, we explored the domain of data analysis and machine learning to provide Jugo with insights into the plausability and possible integration of such predictive AI models within a company.</p>
         </>
     );
 }
