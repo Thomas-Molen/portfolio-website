fb28d39708cb7cf436fda8f06275591b174dec3e
Thomas Molen <sugarypineapple2@gmail.com>
2023-09-03 22:18:31 +0200

    Added BASWorld project description

--- src/components/Projects/BASWorld.tsx
+++ src/components/Projects/BASWorld.tsx
@@ -16,8 +16,16 @@ function BASWorld() {
                 </span>
             </div>
             <p className="project-intro">
+                Developed a fully integrated internal ticketing system within 5 months.
             </p>
-            <p>
+            <p>I had the privilege of leading a collaborative effort to create a comprehensive internal support ticketing system for BAS Trucks, helping the company's transformation into the larger entity known as BAS World.</p>
+            <p>The development of this support ticketing system was a remarkable journey undertaken with a team of dedicated developers. Together, we created a solution that streamlined internal support processes and increased productivity across the organization. This project allowed me to learn a lot as a fresh developer in the enterprise business environment.</p>
+            <p>Notably, the support ticketing system wasn't just limited to our team. It was presented as a challenge to several development teams, with my team being&nbsp;
+                <Underline initialWidth="0">
+                    <a href="https://www.linkedin.com/posts/basworldplatform_werkenbijbas-fontysxbas-fontysict-activity-6811285657755947008-CERL?utm_source=share&utm_medium=member_desktop" target="_blank">
+                        chosen as the favored solution<i className="bi bi-box-arrow-up-right ms-1"></i>
+                    </a>
+                </Underline>.
             </p>
         </>
     );
