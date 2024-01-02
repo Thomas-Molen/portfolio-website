6ed46f480a4f9ecd9e277df364fe1e5320295285
Thomas Molen <sugarypineapple2@gmail.com>
2024-01-02 21:15:33 +0100

    Convert Graduation statistic to tsx
    serverside astro new Date will get the date of the dist build not of the current server time, causing the graduation date to drift by the time since build.
    
    To ensure the days until graduation shows a more accurate value, the component is loaded as a react component and calculated on client side instead

---
+++ src/components/Statistics/Graduation.tsx
@@ -0,0 +1,32 @@
+import "./ProgressBar.css"
+
+interface props {
+    start: Date;
+    end: Date;
+    color: string; // Color of the progress bar
+}
+function Graduation({ start, end, color }: props) {
+    // Calculate the total milisecond range of start and end
+    const range = end.getTime() - start.getTime();
+    // Calculate the current progress in miliseconds
+    const progress = new Date().getTime() - start.getTime();
+    // Convert current progress to %
+    const graduationPercentage = ((progress / range) * 100) + "%";
+
+    // Calculate days left
+    // 1 day = 8.64e+7 ms
+    const daysLeft = Math.round((range - progress) / 8.64e7);
+
+    // styling of the progressbar representing the daysLeft calculated
+    const progressBarStyle = {"--progressColor": color, "--progressPercentage": graduationPercentage} as React.CSSProperties;
+
+    return (
+        <div className="d-flex align-items-center">
+            <b className="me-2">{daysLeft < 0 ? 0 : daysLeft}</b>
+            <div className="progress-bar" style={progressBarStyle}>
+                <div className="progress"></div>
+            </div>
+        </div>)
+}
+
+export default Graduation;
